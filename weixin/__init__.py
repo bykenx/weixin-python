cdaa37e77f643de9ee2b0a57a88d0018296f6ec4
byken <byken@byken.cn>
2020-10-13 02:08:38 +0800

    [refactor]更改目录结构

--- weixin/__init__.py
+++ weixin/__init__.py
@@ -1,8 +1,10 @@
+
+from .base import WeixinError
+from .login import WeixinLogin
 from .msg import WeixinMsg
 from .pay import WeixinPay
-from .login import WeixinLogin
 from .mp import WeixinMP
-from .base import WeixinError
+from .wx import Weixin
 
 
 __all__ = ("Weixin")
